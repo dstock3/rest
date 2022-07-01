33667d12719e421b59867972d26e9d7be77b15f1
David Stockdale <dstock3@gmail.com>
2022-07-01 15:14:57 -0500

    Specified userId in put and delete routes.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+});
+  
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.put('/users/:userId', (req, res) => {
+    return res.send(
+        `PUT HTTP method on user/${req.params.userId} resource`,
+    );
 });
-
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+  
+app.delete('/users/:userId', (req, res) => {
+    return res.send(
+        `DELETE HTTP method on user/${req.params.userId} resource`,
+    );
 });
-
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
+  
+  
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
