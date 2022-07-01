118664266a89c5e4f0b465b9836700fc5e45dd61
David Stockdale <dstock3@gmail.com>
2022-07-01 15:12:11 -0500

    Added routes for get, post, put and delete for users.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello ever running Node.js project.');
+import 'dotenv/config';
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
