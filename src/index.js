9269c014ebba15e4ef6515be63cb40c48f7eb624
David Stockdale <dstock3@gmail.com>
2022-07-02 14:18:06 -0500

    Removed sample data from main index and added models.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import 'dotenv/config';
-
+import models from './models';
 import express from 'express';
 
 const app = express();
@@ -8,29 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
-};
-  
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'By World',
-        userId: '2',
-    },
-};
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
 
 app.use((req, res, next) => {
     req.me = users[1];
