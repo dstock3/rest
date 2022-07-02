e2343c9e715fbfde48bf444d1d982bcf5b21bbcb
David Stockdale <dstock3@gmail.com>
2022-07-02 14:32:50 -0500

    Removed redundant routes in main index.

--- src/index.js
+++ src/index.js
@@ -21,51 +21,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-});
-  
-app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-});
-  
-app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-});
-  
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-});
-  
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-});
-  
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
-        userId: req.context.me.id,
-    };
-
-    req.context.models.messages[id] = message;
-
-    return res.send(message);
-});
-  
-app.delete('/messages/:messageId', (req, res) => {
-    const {
-        [req.params.messageId]: message,
-        ...otherMessages
-    } = req.context.models.messages;
-
-    req.context.models.messages = otherMessages;
-
-    return res.send(message);
-});
-  
-  
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
