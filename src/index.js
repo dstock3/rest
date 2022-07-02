a3236e83ced252491e69db1153a24943de436bbd
David Stockdale <dstock3@gmail.com>
2022-07-02 14:23:19 -0500

    created URIs for each respective route.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 import 'dotenv/config';
 import models from './models';
+import routes from './routes';
 import express from 'express';
 
 const app = express();
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
