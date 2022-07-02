2069d51c2fc0f4406d77cfff7adfef9f8e46f7a2
David Stockdale <dstock3@gmail.com>
2022-07-02 14:20:47 -0500

    Created middleware to pass the models to all routes in a context object.

--- src/index.js
+++ src/index.js
@@ -16,52 +16,47 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    req.me = users[1];
-    next();
-});
-
 app.get('/session', (req, res) => {
-    return res.send(users[req.me.id]);
+    return res.send(req.context.models.users[req.context.me.id]);
 });
   
 app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+    return res.send(Object.values(req.context.models.users));
 });
   
 app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
+    return res.send(req.context.models.users[req.params.userId]);
 });
-
+  
 app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
+    return res.send(Object.values(req.context.models.messages));
 });
-
+  
 app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
+    return res.send(req.context.models.messages[req.params.messageId]);
 });
-
+  
 app.post('/messages', (req, res) => {
     const id = uuidv4();
     const message = {
-      id,
-      text: req.body.text,
-      userId: req.me.id,
+        id,
+        text: req.body.text,
+        userId: req.context.me.id,
     };
-  
-    messages[id] = message;
-  
+
+    req.context.models.messages[id] = message;
+
     return res.send(message);
 });
-
+  
 app.delete('/messages/:messageId', (req, res) => {
     const {
-      [req.params.messageId]: message,
-      ...otherMessages
-    } = messages;
-  
-    messages = otherMessages;
-  
+        [req.params.messageId]: message,
+        ...otherMessages
+    } = req.context.models.messages;
+
+    req.context.models.messages = otherMessages;
+
     return res.send(message);
 });
   
